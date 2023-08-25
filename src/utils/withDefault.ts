61af4e2769b9855acb136a69a5f96cdb6f5f79e7
grezle <4310551+grezle@users.noreply.github.com>
2023-08-25 20:07:29 +0100

    fix: allow withDefault to return false

--- src/utils/withDefault.ts
+++ src/utils/withDefault.ts
@@ -1,14 +1,17 @@
 // Provide a default value and type the response
 export const withDefault = <T>(value: unknown, defaultValue: T) => {
-  // if the default is a number, parse value as int
-  if (typeof defaultValue === "number") {
-    // eslint-disable-next-line no-param-reassign
-    value = parseInt((value as string) || "0", 10);
-  }
   // if default is a bool, prase the bool
   if (typeof defaultValue === "boolean" && typeof value === "string") {
     value = value === "true";
+    // value is cast by check for "true"
+    return value;
   }
+  // if the default is a number, parse value as int
+  if (typeof defaultValue === "number" && typeof value === "string") {
+    // eslint-disable-next-line no-param-reassign
+    value = parseInt((value as string) || "0", 10);
+  }
+
   // return as the same type as defaultValue
   return (value || defaultValue) as T;
 };
